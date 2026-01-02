7c2aed7c369a61313a3f793d7002a07df580882e
115c7a5fac <115c7a5fac+github@pm.com>
2026-01-02 16:53:58 +0100

    Day 5 part 2 not successfull...

--- day5/day5.py
+++ day5/day5.py
@@ -2,7 +2,7 @@
 def get_ranges():
     fresh_id_ranges = []
     ingredient_ids = []
-    with open('./day5_data.txt') as file:
+    with open('./day5_sample.txt') as file:
         is_fresh_id_ranges = True
         for li in file:
             line = li.rstrip()
@@ -16,29 +16,46 @@ def get_ranges():
     return fresh_id_ranges, ingredient_ids
 
 
-def part_1(fir, ii):
+def ranges_to_array(ran):
     ranges = []
-    for r in fir:
-        sp = r.split('-')
-        min = int(sp[0])
-        max = int(sp[1])
-        ranges.append([min, max])
+    for r in ran:
+        start, end = map(int, r.split('-'))
+        ranges.append((start, end))
+    return ranges
+
+
+def part_1(fir, ii):
+    ranges = ranges_to_array(fir)
     valid_ids = []
 
     for i_id in ii:
-        for r in ranges:
-            if i_id <= r[1] and i_id >= r[0]:
+        for start, end in ranges:
+            if i_id <= end and i_id >= start:
                 valid_ids.append(i_id)
 
     valid_ids = set(valid_ids)
     return len(valid_ids)
 
 
+def part_2(ranges):
+    r = ranges_to_array(ranges)
+    sr = sorted(r)
+    merged = []
+    for start, end in sr:
+        if not merged or start > merged[-1][1] + 1:
+            merged.append([start, end])
+        else:
+            merged[-1][1] = max(merged[-1][1], end)
+    total = sum(end - start + 1 for start, end in r)
+    return total
+
+
 def main():
     solution = 0
 
     fresh_id_ranges, ingredient_ids = get_ranges()
-    solution = part_1(fresh_id_ranges, ingredient_ids)
+    #  solution = part_1(fresh_id_ranges, ingredient_ids)
+    solution = part_2(fresh_id_ranges)
 
     print('-->', solution)
 
