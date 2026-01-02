b82e3ff311382f807c27d64aad85aca6edefb682
115c7a5fac <115c7a5fac+github@pm.com>
2026-01-02 11:25:02 +0100

    Day 5

--- day5/day5.py
+++ day5/day5.py
@@ -1,16 +1,47 @@
 
-current = 50
-solution = 0
+def get_ranges():
+    fresh_id_ranges = []
+    ingredient_ids = []
+    with open('./day5_data.txt') as file:
+        is_fresh_id_ranges = True
+        for li in file:
+            line = li.rstrip()
+            if len(line.rstrip()) == 0:
+                is_fresh_id_ranges = False
+            else:
+                if is_fresh_id_ranges:
+                    fresh_id_ranges.append(line)
+                else:
+                    ingredient_ids.append(int(line))
+    return fresh_id_ranges, ingredient_ids
 
-with open('./day1_data.txt') as file:
-    for line in file:
-        li = line.rstrip()
-        direction = li[0]
-        nbr = int(li[1:])
-        current = ((current - nbr) if direction ==
-                   'L' else current + nbr) % 100
 
-        if current == 0:
-            solution += 1
+def part_1(fir, ii):
+    ranges = []
+    for r in fir:
+        sp = r.split('-')
+        min = int(sp[0])
+        max = int(sp[1])
+        ranges.append([min, max])
+    valid_ids = []
 
-print('-->', solution)
+    for i_id in ii:
+        for r in ranges:
+            if i_id <= r[1] and i_id >= r[0]:
+                valid_ids.append(i_id)
+
+    valid_ids = set(valid_ids)
+    return len(valid_ids)
+
+
+def main():
+    solution = 0
+
+    fresh_id_ranges, ingredient_ids = get_ranges()
+    solution = part_1(fresh_id_ranges, ingredient_ids)
+
+    print('-->', solution)
+
+
+if __name__ == "__main__":
+    main()
