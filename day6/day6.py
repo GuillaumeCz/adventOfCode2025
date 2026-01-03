b0e0c69ecb095dfb568eb298f809d0ed80ea2e42
115c7a5fac <115c7a5fac+github@pm.com>
2026-01-03 19:36:25 +0100

    Day 6 part 2

--- day6/day6.py
+++ day6/day6.py
@@ -1,16 +1,13 @@
-import re
-
 
 def get_values():
     with open('./day6_data.txt') as file:
         matrice = []
         operations = []
         for li in file:
-            p = re.sub(r'\ +', ' ', li.rstrip()).split(' ')
-            if p[0].isdigit():
-                matrice.append(p)
+            if '*' in li:
+                operations = li.split()
             else:
-                operations = p
+                matrice.append(li)
 
         return matrice, operations
 
@@ -40,10 +37,54 @@ def part_1(matrice, operations):
     return solution
 
 
+def part_2(matrice, operations):
+    solution = 0
+
+    cols = []
+    for i in range(len(matrice[0])):
+        c = []
+        for j in range(len(matrice)):
+            c.append(matrice[j][i])
+        cols.append(c)
+
+    nbr_char = int((len(cols) / len(operations)))
+
+    cols = cols[:-1][::-1]
+
+    accumulator = []
+    groups_to_combine = []
+    for index, col in enumerate(cols):
+        if col.count(' ') != nbr_char + 1:
+            c = int(''.join(col))
+            accumulator.append(c)
+        else:
+            groups_to_combine.append(accumulator)
+            accumulator = []
+
+        if index == len(cols) - 1:
+            groups_to_combine.append(accumulator)
+
+    operations = operations[::-1]
+    candidates = []
+    for index, groups in enumerate(groups_to_combine):
+        if operations[index] == '+':
+            candidates.append(sum([int(z) for z in groups]))
+        else:
+            acc = 1
+            for j in groups:
+                acc = acc * j
+            candidates.append(acc)
+
+    solution = sum(candidates)
+
+    return solution
+
+
 def main():
     solution = 0
     matrice, operations = get_values()
-    solution = part_1(matrice, operations)
+    # solution = part_1(matrice, operations)
+    solution = part_2(matrice, operations)
     print('-->', solution)
 
 
